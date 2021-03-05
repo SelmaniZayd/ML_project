42a6cdf43029653f100b16f54d1128e6166f5a74
zayd selmani <selmani.zayd@gmail.com>
2021-03-05 16:32:50 +0100

    ouijdane's updates on front

--- web/front/src/App.js
+++ web/front/src/App.js
@@ -3,12 +3,22 @@ import './assets/CSS/App.css';
 import Footer from './components/Footer';
 import Forms from './components/Forms';
 import Header from './components/Header';
+import { useState } from 'react';
+
 
 const App = () => {
+  const [variable, functionChangeVariable] = useState(false)
 
   return <>
-    {/* <Header/> */}
-    <Forms/>
+    <Header/>
+      {!variable && <div className="content-button">
+        <button onClick={()=>functionChangeVariable(true)}>
+        Accéder au formulaire
+      </button> 
+      </div>}
+    {variable && <Forms/>}
+
+    {/* <Results/> */}
     {/* <Footer/> */}
   </>
 }
--- web/front/src/components/Header/index.jsx
+++ web/front/src/components/Header/index.jsx
@@ -1,5 +1,7 @@
-const Header = () => {
-
-}
+import Icone from "../../assets/Img/logo.svg"
+const Header = () => <header>
+    <h1>HR Analytics</h1>
+    <img src={Icone} alt="icon-hr-analytics" className="icone-header" />
+</header>
 
 export default Header;
