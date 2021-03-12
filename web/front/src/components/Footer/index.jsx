57a171030ce8160bfb387a46c59240bd53ded1f3
zayd selmani <selmani.zayd@gmail.com>
2021-03-12 15:18:33 +0100

    updating git repo

--- web/front/src/components/Footer/index.jsx
+++ web/front/src/components/Footer/index.jsx
@@ -19,7 +19,7 @@ const Footer = () => <div className="footer">
     <div className="equipe">
         <p className="title-equipe">Lien: </p>
         <div>
-            <p><a href="https://github.com/Ouijdane110/Deep-learning" target="_blank">Github</a></p>
+            <p><a href="https://github.com/SelmaniZayd/ML_project" target="_blank">Github</a></p>
             <p><a href="https://onedrive.live.com/view.aspx?resid=6C6D756296D4662!720129&ithint=file%2cdocx&authkey=!ALyigml2648FXRY" target="_blank">Sujet du projet</a></p>
         </div>
     </div>
